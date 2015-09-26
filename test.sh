1d3f889a1b5c51e71cd6881145037ef17697698a
Michael Casadevall <michael@mcprohosting.com>
2015-09-26 03:37:35 -0400

    Turn off verbose tests by default
    Signed-off-by: Michael Casadevall <michael@mcprohosting.com>

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # We have to ignore warnings because mysql.connector's pool doesn't have a method to close cleanly
-sudo PYHTONPATH=`pwd` python3 -W ignore -m unittest discover -v -s ./tests -p "*.py"
+sudo PYHTONPATH=`pwd` python3 -W ignore -m unittest discover -s ./tests -p "*.py"
